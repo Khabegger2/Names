49d68d432d0856a2fc8f5ddaea7b0f36145d81ee
raskew <35855572+raskew@users.noreply.github.com>
2018-01-26 21:03:04 -0600

    Initial Commit

---
+++ Names.c
@@ -0,0 +1,13 @@
+/**
+* Authors: Ryan Askew and Kellen Habegger
+* Date: January 24, 2018
+*
+*
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main(int argc, char **argv) {
+	printf("Ryan Askew\n");
+}
